be22032f14765c71dd2ae2ea86b15ab71515314c
Aleksandr Karelin <alexander.karelin@videinfra.com>
2026-05-20 16:19:12 +0700

    Introduce `Router` class, refactor `App` to enable route handling and dispatch mechanism

--- app/Core/App.php
+++ app/Core/App.php
@@ -14,10 +14,17 @@ class App
     public function run(): void
     {
         $pdo = ConnectionFactory::getPDO();
-        $categoryRepo = new CategoryRepository($pdo);
-        $articleRepo = new ArticleRepository($pdo);
+        $view = new View();
 
-        $homeController = new HomeController(new View(), $categoryRepo, $articleRepo);
-        $homeController->index();
+        $categoryRepository = new CategoryRepository($pdo);
+        $articleRepository = new ArticleRepository($pdo);
+
+        $router = new Router();
+
+        $router->get('/', [new HomeController($view, $categoryRepository, $articleRepository), 'index']);
+//        $router->get('/category', [new CategoryController($view, $categoryRepository, $articleRepository), 'show']);
+//        $router->get('/article', [new ArticleController($view, $categoryRepository, $articleRepository), 'show']);
+
+        $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
     }
 }
--- app/Core/Router.php
+++ app/Core/Router.php
@@ -4,7 +4,29 @@ declare(strict_types=1);
 
 namespace Abelohost\TestApp\Core;
 
+use Closure;
+
 class Router
 {
+    private array $routes = [];
 
+    public function get(string $path, Closure|array $handler): void
+    {
+        $this->routes['GET'][$path] = $handler;
+    }
+
+    public function dispatch(string $method, string $uri): void
+    {
+        $path = parse_url($uri, PHP_URL_PATH);
+
+        $handler = $this->routes[$method][$path] ?? null;
+
+        if ($handler === null) {
+            http_response_code(404);
+            echo '404 Not Found';
+            return;
+        }
+
+        call_user_func($handler);
+    }
 }
